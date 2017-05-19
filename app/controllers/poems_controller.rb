1242b2f6927faef74a0d0a494183cb11422874d6
swmemo <s.watanabes@gmail.com>
2017-05-19 13:06:45 +0000

    “mm”

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,11 +1,13 @@
 class PoemsController < ApplicationController
   def index
     @poems = Poem.all
+    render json: @blogs
   end
   
   # dive14課題
   def show
     @poem = Poem.find(params[:id])
+    render json: @poems
   end
 
 end
